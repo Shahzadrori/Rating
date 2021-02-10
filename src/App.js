cca13f3440827526da4335826af42bdc1c5a3865
shahzadrori <shahzadrori@gmail.com>
2021-02-10 15:57:40 +0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App=()=>{
+  const Url= "https://picsum.photos/300/400";
+return(
+  <>
+    <div className='main'>
+    <img src={Url} alt="Image" />
+    <ul className='star'>
+
+    </ul>
     </div>
-  );
+  </>
+)
 }
-
-export default App;
+export default App
