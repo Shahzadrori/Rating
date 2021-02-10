a02db1874811270194f35609e518a1b8c813cdeb
shahzadrori <shahzadrori@gmail.com>
2021-02-10 20:12:17 +0500

    styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './App.css'
 
 const App=()=>{
   const Url= "https://picsum.photos/300/400";
@@ -7,9 +7,15 @@ return(
   <>
     <div className='main'>
     <img src={Url} alt="Image" />
-    <ul className='star'>
-
+    <ul className='stars'>
+     <li className='star'><i class="fa fa-thumbs-up"></i></li>
+     <li className='star'><i class="fa fa-thumbs-up"></i></li>
+     <li className='star'><i class="fa fa-thumbs-up"></i></li>
+     <li className='star'><i class="fa fa-thumbs-up"></i></li>
+     <li className='star'><i class="fa fa-thumbs-up"></i></li>
     </ul>
+    <h3>ysadfkl</h3>
+    <button>Click Me</button>
     </div>
   </>
 )
