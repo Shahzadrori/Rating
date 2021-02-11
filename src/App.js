084d4ef882f081e263b566e0e10784130368d761
shahzadrori <shahzadrori@gmail.com>
2021-02-11 10:55:47 +0500

    styling

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from 'react'
 import './App.css'
 
 const App=()=>{
-  const Url= "https://picsum.photos/300/400";
+  const Url= "https://picsum.photos/300/300";
 return(
   <>
     <div className='main'>
     <h2>Tell us how you like it</h2>
     <img src={Url} alt="Image" />
     <ul className='stars'>
-     <li className='star'><i class="likee fa fa-thumbs-up"></i></li>
-     <li className='star'><i class="likee fa fa-thumbs-up"></i></li>
-     <li className='star'><i class="likee fa fa-thumbs-up"></i></li>
-     <li className='star'><i class="likee fa fa-thumbs-up"></i></li>
-     <li className='star'><i class="likee fa fa-thumbs-up"></i></li>
+     <li className='star'><i className="likee fa fa-star"></i></li>
+     <li className='star'><i className="likee fa fa-star"></i></li>
+     <li className='star'><i className="likee fa fa-star"></i></li>
+     <li className='star'><i className="likee fa fa-star"></i></li>
+     <li className='star'><i className="likee fa fa-star"></i></li>
     </ul>
     <h3>ysadfkl</h3>
     <button>Click Me</button>
